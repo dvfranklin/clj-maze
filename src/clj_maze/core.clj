63248ec381f72d65319ef66e3535c607c263a372
Dan <dvfranklin@gmail.com>
2016-06-09 12:36:39 -0400

    Endpoint printing

--- src/clj_maze/core.clj
+++ src/clj_maze/core.clj
@@ -44,38 +44,54 @@
     (> new-col old-col)
     (assoc-in rooms [old-row old-col :right?] false)))
 
-(defn add-endpoint [rooms row col]
-  (let [endpoints     (for [x rooms] (map :end? x))
-        truefalse     (for [x endpoints] (some true? x))
-        rooms         (if (not (some true? truefalse)) (assoc-in rooms [1 1 :end?] true))]
-  rooms))
+(defn endpoint-reached [rooms]
+  (> (count
+       (set
+         (for [row rooms
+               room row]
+           (:end? room)))) 1))
 
 
-;(defn create-maze-loop [rooms old-row old-col new-row new-col]
-;  (let [new-rooms (tear-down-wall rooms old-row old-col new-row new-col)
-;        new-rooms (create-maze new-rooms new-row new-col)]
-;    (if (= rooms new-rooms)
-;      (let [rooms   (assoc-in rooms [old-row old-col :end?] true)]
-;        rooms)
-;      (create-maze-loop new-rooms old-row old-col new-row new-col))))
 
-
-(defn create-maze [rooms row col]
-  (let [rooms (assoc-in rooms [row col :visited?] true)
-        next-room (random-neighbor rooms row col)]
+(defn create-maze [rooms row col first?]
+  (let [rooms       (assoc-in rooms [row col :visited?] true)
+        next-room   (random-neighbor rooms row col)
+        rooms       (if (and (nil? next-room) (not (endpoint-reached rooms)))
+                       (assoc-in rooms [row col :end?] true)
+                       rooms)
+        rooms (if first? (assoc-in rooms [row col :start?] true)
+                         rooms)]
     (if next-room
       (loop [old-rooms (tear-down-wall rooms row col (:row next-room) (:col next-room))]
-        (let [new-rooms (create-maze old-rooms (:row next-room) (:col next-room))]
+        (let [new-rooms (create-maze old-rooms (:row next-room) (:col next-room) false)]
           (if (= old-rooms new-rooms)
             old-rooms
             (recur new-rooms))))
       rooms)))
 
 
+(defn print-room [room]
+  ; start / bottom / end check
+  (cond
+    (and (:bottom? room) (:start? room))
+    (print ">")
+    (:start? room)
+    (print "v")
+    (and (:bottom? room) (:end? room))
+    (print "±")
+    (:end? room)
+    (print "+")
+    (:bottom? room)
+    (print "_")
+    :else (print " "))
+  ; either wall or not
+  (if (:right? room)
+    (print "|")
+    (print " ")))
+
 (defn -main []
   (let [rooms (create-rooms)
-        rooms (assoc-in rooms [0 0 :start?] true)
-        rooms (create-maze rooms 0 0)]
+        rooms (create-maze rooms 0 0 true)]
 
     ; print top walls
     (doseq [_ rooms]
@@ -85,15 +101,5 @@
     (doseq [row rooms]
       (print "|")
       (doseq [room row]
-        (cond
-          (:start? room)
-          (print "o")
-          (:end? room)
-          (print "x")
-          (:bottom? room)
-          (print "_")
-          :else (print " "))
-        (if (:right? room)
-          (print "|")
-          (print " ")))
-      (println))))
+        (print-room room))
+        (println))))
